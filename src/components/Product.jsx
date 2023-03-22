37076be46eabb17bc17c0c38b84e3df180271860
josua-stng <josuasitanggang51@gmail.com>
2023-03-22 11:28:44 +0700

    create routing cart

--- src/components/Product.jsx
+++ src/components/Product.jsx
@@ -3,14 +3,17 @@ import { Link } from "react-router-dom";
 
 const Product = ({ title, image, price, description ,productId}) => {
   return (
-    <Link to={`/product/${productId}`}>
+    <>
     <div className="bg-blue-200 p-6 ">
+    <Link to={`/product/${productId}`}>
       <img className=" w-full h-48 " src={`${image}`} alt="image_photos" />
       <h2 className="font-bold text-xl mb-2 line-clamp-1">{title}</h2>
       <p className="text-blue-800 font-bold mb-2">$ {price}</p>
       <p className="line-clamp-3">{description}</p>
-    </div>
+  
     </Link>
+    </div>
+    </>
   );
 };
 
